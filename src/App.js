bb1468638bafcd91b99ec44271df24332c0860da
carojp <carojp62@gmail.com>
2023-01-16 16:22:55 -0500

    commit imprimir name y url

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [character, setCharacter] = useState({
+    results: [
+      {
+        name: "",
+        url: "",
+      },
+    ],
+  });
+
+  useEffect(() => {
+    fetch(`https://pokeapi.co/api/v2/pokemon`)
+      .then((resp) => resp.json())
+      .then((data) => {
+        setCharacter(data);
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p> Nombre: {character.results.name} </p>
+      <p> url: {character.results.url} </p>
     </div>
   );
 }
